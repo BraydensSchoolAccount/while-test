af88936a8b9d0905cee6702c78deaff7d4bf2634
BraydensSchoolAccount <bt22219@students.hcde.org>
2024-02-08 08:04:28 -0500

    More questions answered

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ while number <=10:
 #     number -= 1
 
 # Output: Decrementing negative numbers
+# The number, continually going down, never met the condition
+# It doesn't end because it never meets the condition of the while loop, running forever
+
+number = 1
+while number <=10:
+    if number % 2 == 0:
+        print(number, end=" ")
+    number += 1
+
+# Output: 2 4 6 8 10
+# The end being " " causes each item to be separated by a space
+# Line 3 checks to see if the number is a multiple of 2
+
+while valid_input:
+    user_number = int(input("Enter a number between 1 and 10 >  "))
+    valid_input = 1 <= user_number <= 10
+    if not valid_input:
+        print("Invalid input, please try again")
+print("Valid input, you're clear")
