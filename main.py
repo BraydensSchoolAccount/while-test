373b1359578254b8f69fe8f8085952ccc2be6d09
BraydensSchoolAccount <bt22219@students.hcde.org>
2024-02-08 07:55:27 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# While Loop test
+# Brayden Towner
+# 02/08/2024
+
+# FIX previous number value was 12, making the while invalid
+number = 1
+while number <=10:
+    print(number)
+    number += 1
+
+# DON'T RUN
+# number = 0
+# while number <=10:
+#     print(number)
+#     number -= 1
+
+# Output: Decrementing negative numbers
